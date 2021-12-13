64cb240216252bccb4d331a0907f47bd8e44a654
terrarier2111 <58695553+terrarier2111@users.noreply.github.com>
2021-12-13 08:55:56 +0100

    Update decoder.rs

--- src/decoder.rs
+++ src/decoder.rs
@@ -1,12 +1,15 @@
+
 use crate::utils::{DataComponent, DataTypeMethods, DataContainer};
 use std::any::Any;
 use std::io::Read;
 use crate::connection::Connection;
 use crate::buffer::Buffer;
+use crate::appliaction::Application;
 
 pub struct Decoder {
 
-
+    application: Application,
+    config: DecoderConfig,
 
 }
 
@@ -19,12 +22,14 @@ pub(crate) struct DecoderConfig {
 
 pub(crate) struct DecoderState {
 
+    connection: Connection,
     stored_data: DataContainer,// Vec<DataComponent<Box<dyn DataTypeMethods + 'static>>>,
     index: u32,
     has_id: bool,
     invalid_data: bool,
     last_keep_alive_id: u8,
     read_keep_alive_id: bool,
+    framing: FramingState,
 
 }
 
@@ -38,7 +43,8 @@ pub(crate) struct FramingState {
 
 impl Decoder {
 
-    pub fn decode(&mut self, connection: Connection, buffer: Buffer) {
+    // FIXME: Return result with IOError and some other type or `()`
+    pub fn decode(&self, state: &mut DecoderState, buffer: &mut Buffer) {
 
     }
 
